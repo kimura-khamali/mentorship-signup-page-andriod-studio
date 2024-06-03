2c940bf8d3e4979781baa360a99b6524ec6bdac0
kimura-khamali <kimurakhamali@gmail.com>
2024-06-03 14:22:15 +0300

    commit

--- app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
@@ -4,19 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-       val btnNext=findViewById<Button>(R.id.btnNext)
-        btnNext.setOnClickListener{
+       val log=findViewById<TextView>(R.id.log)
+           log.setOnClickListener{
             val intent=Intent(this,loginactivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/akirachix/mentorconnect/loginactivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/loginactivity.kt
@@ -1,16 +1,25 @@
 package com.akirachix.mentorconnect
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class loginactivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_loginactivity)
+        val sign=findViewById<TextView>(R.id.sign)
+        sign.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
