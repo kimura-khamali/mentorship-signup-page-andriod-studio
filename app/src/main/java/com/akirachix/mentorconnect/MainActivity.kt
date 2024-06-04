c5ff50b90d6648001bbabcf2c3fdfb92fc62d3a9
kimura-khamali <kimurakhamali@gmail.com>
2024-06-04 22:37:48 +0300

    commit

--- app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
@@ -9,17 +9,87 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
+    lateinit var binding: ActivityMainBinding
+
+    //    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-       val log=findViewById<TextView>(R.id.log)
-           log.setOnClickListener{
-            val intent=Intent(this,loginactivity::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.log.setOnClickListener {
+            val intent = Intent(this, loginactivity::class.java)
             startActivity(intent)
         }
+        binding.btnNext.setOnClickListener {
+            validateRegistration()
+        }
+
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        val log = findViewById<TextView>(R.id.log)
+//        log.setOnClickListener {
+//            val intent = Intent(this, loginactivity::class.java)
+//            startActivity(intent)
+//        }
+
     }
+
+    fun validateRegistration() {
+        var formErr = false
+//        var clearErrors =
+        val firstName = binding.etfirstname.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tifirstname.error = "First name is required"
+        }
+
+        val lastName = binding.etlastname.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tilastname.error = "Last name is required"
+        }
+        val email = binding.etemail.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tifirstname.error = "Email  is required"
+        }
+        val codehiveid = binding.etid.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tifirstname.error = "Codehive ID is required"
+        }
+        val username = binding.etusername.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tifirstname.error = "Username is required"
+        }
+        val password = binding.etconpassword.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tipassword.error = "Password is required"
+        }
+        val passwordConf = binding.etconpassword.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tipassword.error = "Password is required"
+        }
+        if (password != passwordConf) {
+            formErr = true
+            binding.ticonpassword.error = "Password don't match"
+    }
+        if (!formErr) {
+            //proceed to registration
+        }
+}
+//    fun clearErrors(){
+//        binding.tifirstname.error = null
+//        binding.tilastname.error = null
+//        binding.etemail.error = null
+////        binding.ticonpassword = null
+//        binding.
+//    }
 }
--- app/src/main/java/com/akirachix/mentorconnect/loginactivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/loginactivity.kt
@@ -8,22 +8,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivityLoginactivityBinding
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class loginactivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginactivityBinding
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding.sign.setOnClickListener {
+//            startActivity(Intent(this,MainActivity::class.java))
+//        }
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_loginactivity)
-        val sign=findViewById<TextView>(R.id.sign)
-        sign.setOnClickListener{
-            val intent= Intent(this,MainActivity::class.java)
+        binding = ActivityLoginactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.sign.setOnClickListener {
+            val intent = Intent(this, loginactivity::class.java)
             startActivity(intent)
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.button2.setOnClickListener {
+            validateRegistration()
         }
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_loginactivity)
+//        val sign=findViewById<TextView>(R.id.sign)
+//        sign.setOnClickListener{
+//            val intent= Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//       }
+    }
+
+    fun validateRegistration() {
+        var formErr = false
+//        clearErrors()
+        val firstName = binding.etuserlogin.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tiluserlogin.error = "User Name is required"
+        }
+
+        val lastName = binding.etpasswordlogin.text.toString()
+        if (firstName.isBlank()) {
+            formErr = true
+            binding.tilpasswordlogin.error = "Last name is required"
+        }
+        }
+    fun clearError() {
+        
     }
 }
